09bc6c0f92e9e1d7985acc52f0adb4ce7a47dcf2
mena996 <menaemad996@gmail.com>
2020-03-15 20:08:39 +0200

    fix integration problems

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,6 @@ class CreateUsersTable extends Migration
             $table->string('image')->default('user.jpg');
             $table->boolean('isActive')->default(true);
             $table->boolean('isAdmin')->default(false);
-
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
