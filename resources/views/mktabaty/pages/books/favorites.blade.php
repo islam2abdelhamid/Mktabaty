6a5c1b7ba0bafaca01a78f1b4da05f8ae26205c5
ahmedatef00 <ahmed62437@student.must.edu.eg>
2020-03-16 22:06:16 +0200

    list favourite books

--- resources/views/mktabaty/pages/books/favorites.blade.php
+++ resources/views/mktabaty/pages/books/favorites.blade.php
@@ -9,12 +9,12 @@
 <div class="col-md-4 mt-4">
   <div class="card card-plain">
 
-    <img class="card-img-top" src="assets/user/images/got.jpg" alt="book image" height="250px" />
+  <img class="card-img-top" src="{{ $book->image }}" alt="book image" height="250px" />
 
     <div class="card-body">
       <div class="d-flex justify-content-between">
         <h4 class="card-title">
-          <a href="/book" class="no-decoration">Book Title</a>
+          <a href="/book" class="no-decoration">{{ $book->title }}</a>
         </h4>
         <i class="fa fa-heart-o fa-pull-right mb-3" aria-hidden="true"></i>
 
@@ -27,8 +27,8 @@
         <span class="fa fa-star"></span>
       </div>
       <div class="d-flex justify-content-between align-items-center mb-3">
-        <span class="text-secondary d-flex ">4 copies available</span>
-        <span class="badge badge-pill badge-warning">36EGP</span>
+        <span class="text-secondary d-flex ">{{ $book->quantity }}</span>
+        <span class="badge badge-pill badge-warning">{{ $book->price }}</span>
       </div>
 
       <p class="card-text">
