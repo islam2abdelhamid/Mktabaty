97135623f148c283e1eb69e4112ee559acd11ea3
Islam <iabdelhamid3@gmail.com>
2020-03-17 16:47:12 +0200

    fix some errors

--- app/Comment.php
+++ app/Comment.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use SoftDeletes;
     protected $fillable = ['user_id','comment','rate','book_id'];
+
+    
     public function book()
     {
         return $this->belongsTo('App\Book');
